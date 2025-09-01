c83c39c5ab815c6a432ac20c726ed1c30cd8d4e5
qoyyum <odubajoq@yahoo.com>
2025-09-01 12:58:12 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ const port = 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.post('/motivate', async (req, res) => {
     const { feeling, responseType } = req.body;
     const apiKey = process.env.API_KEY;
